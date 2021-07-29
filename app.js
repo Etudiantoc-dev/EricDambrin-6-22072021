71e1d8237423f9d74f08bdd205e68d9a2aa20be4
Eric <e.dambrin@gmail.com>
2021-07-29 11:44:42 +0200

    remplacement express.json au lieu de bodyParser.json

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');// Pour gérer la demande post en important bodyParser ici
+// const bodyParser = require('body-parser');// Pour gérer la demande post en important bodyParser ici
 const mongoose = require('mongoose');
 
 const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
 const path = require('path');
+const app = express();
 
 
 mongoose.connect('mongodb+srv://Eric:Dbmongoose@cluster0.1da6l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
@@ -14,18 +15,15 @@ mongoose.connect('mongodb+srv://Eric:Dbmongoose@cluster0.1da6l.mongodb.net/myFir
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
-
-const app = express();
-
-
 app.use((req, res, next) => {//Pour éviter l'erreur CORS = méthode de sécurité par défault //Configurer les bons headers sur l'objet réponse permet l'envoi et la réception de requètes et de réponses sans erreurs CORS
     res.setHeader('Access-Control-Allow-Origin', '*');//Accés à l'API depuis n'importe quelle origine
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');//Ajouter les Headers mentionnés aux requêtes
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');//Pour rendre possible l'envoie des requêtes avec les méthodes mentionnées(get,post...)
     next();
   });
-
-  app.use(bodyParser.json());//définit la fontion json comme middleware global pour l'application
+ 
+  // app.use(bodyParser.json());
+  app.use(express.json());//définit la fonction json comme middleware global pour l'application
   
   app.use('/images', express.static(path.join(__dirname, 'images')));
 
